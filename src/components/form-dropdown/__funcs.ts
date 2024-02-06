b2bdda4a771d063b93428d7485bf5c9d391e366b
Edgar Juvianno Santoso <edgarjuvianno2@gmail.com>
2024-02-06 20:48:20 +0700

    fix: fix render label dropdown

--- src/components/form-dropdown/__funcs.ts
+++ src/components/form-dropdown/__funcs.ts
@@ -1,5 +1,5 @@
 export const renderOption = (opt: any, renderLabel: any) => {
-	if (opt) {
+	if (opt && typeof opt === 'object') {
 		if (typeof renderLabel === 'string') {
 			return opt[renderLabel]
 		}
