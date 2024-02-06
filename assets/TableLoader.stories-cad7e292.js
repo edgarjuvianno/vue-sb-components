04b49e0b9bfa51655c45cfe4136af28a3e95266f
Edgar Juvianno Santoso <edgarjuvianno2@gmail.com>
2024-02-06 21:04:38 +0700

    Updates

---
+++ assets/TableLoader.stories-cad7e292.js
@@ -0,0 +1,5 @@
+import{T as e,D as n}from"./datatable-93737389.js";import"./vue.esm-bundler-69a57796.js";import"./form-dropdown-b91d0a88.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./progress-circular-71f86238.js";import"./form-input-e7cfc306.js";import"./icons-70109494.js";import"./helper-91f3687e.js";const h={title:"Components/Table Loader",component:e,tags:["autodocs"],argTypes:{rowCount:{control:"number",description:"Table row count"}},render:s=>({components:{Datatable:n,TableLoader:e},setup(){return{args:s}},template:'<Datatable><template v-slot:header><tr><th>A</th><th>B</th><th>C</th><th>D</th></tr></template><template v-slot:body><TableLoader v-bind="args" /></template></Datatable>'})},t={args:{rowCount:10}};var o,r,a;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    rowCount: 10
+  }
+}`,...(a=(r=t.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const D=["Default"];export{t as Default,D as __namedExportsOrder,h as default};
