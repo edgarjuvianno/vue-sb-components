688479bf2630388e5e386056c482f28e2538a703
Edgar Juvianno Santoso <edgarjuvianno2@gmail.com>
2024-05-21 15:53:07 +0700

    fix: fix org chart styling

--- src/stories/components/OrganizationTree.stories.ts
+++ src/stories/components/OrganizationTree.stories.ts
@@ -52,7 +52,7 @@ const meta = {
 					x: 250,
 					y: 10,
 				},
-				name: 'Nicolas Saputra hehehehehehehe',
+				name: 'Nicolas Saputra hehehehehehehe alsdkjasjdklasdjajsdjklajsdalsjdjalsjdajdalsjdalksjdalkjsdklajsdljasldjaslkjdasdajsldkjasldjaskldjlasldkjasldas',
 				photo: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww',
 				position:
 					'alsdkjasjdklasdjajsdjklajsdalsjdjalsjdajdalsjdalksjdalkjsdklajsdljasldjaslkjdasdajsldkjasldjaskldjlasldkjasldas',
